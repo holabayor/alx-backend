b1eb92685f477c05b61124c8232f1ede44d2c0a1
holabayor <liasu.olabayor@gmail.com>
2023-01-22 16:41:22 +0100

    doc fixed

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -36,6 +36,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''
+            paginate pages
+        '''
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
 
