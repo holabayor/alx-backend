ab6a8aafc7120a32f8869f6066629ce829f2dbf1
holabayor <liasu.olabayor@gmail.com>
2023-01-22 16:49:18 +0100

    lexical error fixed

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -59,7 +59,7 @@ class Server:
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
         next_page = page + 1 if page_size > page + 1 else None
-        prev_page = page - 1 if page_size < page - 1 else None
+        prev_page = page - 1 if page_size < 1 else None
         return {
             'page_size': len(data),
             'page': page,
