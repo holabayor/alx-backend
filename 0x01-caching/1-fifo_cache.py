562887f9eb93e1f33fc967824897b24f792ea79f
holabayor <liasu.olabayor@gmail.com>
2023-01-25 23:57:09 +0100

    logic

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''Basic Dictionary'''
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''
+        A Basic Cache System
+    '''
+
+    def put(self, key, item):
+        '''
+            Method to assign to the dictionary self.cache_data
+            the item value for the key key
+        '''
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''
+            Method to return the value in self.cache_data linked to key
+        '''
+        return self.cache_data.get(key, None)
