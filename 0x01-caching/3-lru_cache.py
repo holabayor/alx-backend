c157718f83160062a17db6b9e7d05bc9d8b1d7f9
holabayor <liasu.olabayor@gmail.com>
2023-01-26 01:25:58 +0100

    lru and mru fix

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-'''LRU Cache'''
+'''MRU Cache'''
 from collections import OrderedDict, deque
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
+class MRUCache(BaseCaching):
     '''
-        A LRU Cache System
+        A MRU Cache System
     '''
     def __init__(self):
         super().__init__()
-        self.lru_keys = deque([])
+        self.keys = deque([])
 
     def put(self, key, item):
         '''
@@ -19,20 +19,20 @@ class LRUCache(BaseCaching):
         '''
         if key and item:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS\
-                    and key not in self.lru_keys:
-                discard_key = self.lru_keys.popleft()
+                    and key not in self.keys:
+                discard_key = self.keys.popleft()
                 del self.cache_data[discard_key]
                 print('DISCARD: {}'.format(discard_key))
-            if key in self.lru_keys:
-                self.lru_keys.remove(key)
+            if key in self.keys:
+                self.keys.remove(key)
             self.cache_data[key] = item
-            self.lru_keys.append(key)
+            self.keys.append(key)
 
     def get(self, key):
         '''
             Method to return the value in self.cache_data linked to key
         '''
-        if key in self.lru_keys:
-            self.lru_keys.remove(key)
-            self.lru_keys.append(key)
+        if key in self.keys:
+            self.keys.remove(key)
+            self.keys.append(key)
         return self.cache_data.get(key, None)
