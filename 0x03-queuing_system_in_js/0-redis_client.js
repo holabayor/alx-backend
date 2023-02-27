2706e8c7b28497da3fcfb3282b380e28de48a005
holabayor <liasu.olabayo@gmail.com>
2023-02-27 19:41:31 +0100

    eslint fixed

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,3 +1,5 @@
 import { createClient } from 'redis';
 
-createClient().on('error', (err) => console.log('Redis client not connected to the server:', err)).on('connect', () => console.log('Redis client connected to the server'));
+createClient()
+  .on('error', (err) => console.log('Redis client not connected to the server:', err))
+  .on('connect', () => console.log('Redis client connected to the server'));
