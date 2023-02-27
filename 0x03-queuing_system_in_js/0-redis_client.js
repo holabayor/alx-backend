c90f787d2d9eb118a66781189a271b09116a3208
holabayor <liasu.olabayo@gmail.com>
2023-02-27 19:40:11 +0100

    task 1

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,3 @@
+import { createClient } from 'redis';
+
+createClient().on('error', (err) => console.log('Redis client not connected to the server:', err)).on('connect', () => console.log('Redis client connected to the server'));
