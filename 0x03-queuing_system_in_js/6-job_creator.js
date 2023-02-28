2e09696af3e69e94918d995c87b67aa0339918a6
holabayor <liasu.olabayo@gmail.com>
2023-02-28 14:21:21 +0100

    task 6

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -10,10 +10,9 @@ const queue = kue.createQueue();
 const job = queue.create('push_notification_code', objectData).save();
 
 job.on('enqueue', () => {
-    console.log(`Notification job created: ${job.id}`);
-  }).on('complete', () => {
-    console.log('Notification job completed');
-  }).on('failed', () => {
-    console.log('Notification job failed');
-
-  });
+  console.log(`Notification job created: ${job.id}`);
+}).on('complete', () => {
+  console.log('Notification job completed');
+}).on('failed', () => {
+  console.log('Notification job failed');
+});
