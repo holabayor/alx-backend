34dd928c41754fb9371e79f9b96ab5100fcadd91
holabayor <liasu.olabayo@gmail.com>
2023-02-28 14:57:14 +0100

    tasks 8 and 9

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -49,17 +49,16 @@ const jobs = [
 
 const queue = kue.createQueue();
 
-jobs.forEach((objectData, index) => {
+jobs.forEach((objectData) => {
   const job = queue.create('push_notification_code_2', objectData).save();
-  const progress = ((index + 1) / jobs.length) * 100;
 
   job.on('enqueue', () => {
     console.log(`Notification job created: ${job.id}`);
   }).on('complete', () => {
-    console.log('Notification job completed');
-  }).on('failed', () => {
-    console.log('Notification job failed');
-  }).on('progress', () => {
-    console.log(`Notification job ${progress}% complete`);
+    console.log(`Notification job ${job.id} completed`);
+  }).on('failed', (error) => {
+    console.log(`Notification job ${job.id} failed: ${error}`);
+  }).on('progress', (progress) => {
+    console.log(`Notification job ${job.id} ${progress}% complete`);
   });
 });
